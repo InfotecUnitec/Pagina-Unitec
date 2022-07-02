2df26cdb77d99953cbff31e9cbf362282444d076
Camilo <camilodipaolo10@gmail.com>
2022-07-02 12:39:46 -0300

    Primer version de la pagina

---
+++ js/script-sectores.js
@@ -0,0 +1,75 @@
+const src = [
+  "Late.jpg",
+  "Late.jpg",
+  "Sistema2.jpg",
+  "Late.jpg",
+  "logolabtic.png",
+  "Infotec.png",
+];
+const cards = [
+  "Late",
+  "EDETEC y Proyecto Arduino",
+  "Técnicas 3D",
+  "Mecatrónica",
+  "LabTIC",
+  "InfoTEC",
+];
+const descripciones = [
+  "Laboratorio de asistencia Técnica a establecimientos de educación Especial.",
+  "Espacio de desarrollo de rampas tecnológicas para la accesibilidad e inclusión de PcD",
+  "Este espacio cuenta con 2 Impresoras 3D: una Prusa i3 y una Makerbot replicador mini; un Scanner 3D y un CNC",
+  "En este espacio se desarrolla fundamentalmente trabajos bajo ARDUINO, en lo referente a automatización del hogar ",
+  "Laboratorio de desarrollo de Hardware y Software con orientación al uso de TIC`s..",
+  "Espacio de extensión dedicado al desarrollo de software",
+];
+const cardsContainer = document.querySelector(".carrousel-items");
+let actualCard = 0;
+
+const templateCard = (label, id, src, descripcion) => {
+  return `<div class="carrousel-item">
+  <img src="/src/img/${src}" />
+  <div class="info-card">
+  <h3>${label}</h3>
+  <div class="info-container">
+  <p class="Informacion">
+  ${descripcion}
+  </p>
+  </div>
+  </div>
+  <div class="click-area" data-card="${id}"></div>
+  </div>`;
+};
+
+// cargamos todas las tajetas
+cards.forEach((cardLabel, i) => {
+  console.log(`../img/${src[i]}`);
+  cardsContainer.innerHTML += templateCard(
+    cardLabel,
+    i,
+    src[i],
+    descripciones[i]
+  );
+});
+
+const cardsHTML = [...document.querySelectorAll(".carrousel-item")];
+console.log(cardsHTML, cardsHTML.length);
+
+// cargamos el estado inicial del carrousel
+cardsHTML[0]?.classList.add("center");
+cardsHTML[1]?.classList.add("rigth");
+
+cardsContainer.addEventListener("click", (e) => {
+  const cardId = e.target.dataset.card * 1;
+  // evitamos que se rompa si el usuario da muchos clicks
+  if (!cardId && cardId != 0) return;
+
+  cardsHTML.forEach((card, i) => {
+    card.classList.remove("center");
+    card.classList.remove("left");
+    card.classList.remove("rigth");
+  });
+
+  cardsHTML[cardId]?.classList.add("center");
+  cardsHTML[cardId + 1]?.classList.add("rigth");
+  cardsHTML[cardId - 1]?.classList.add("left");
+});
---
+++ js/script.js
@@ -0,0 +1,79 @@
+window.addEventListener("scroll", (e) => {
+  document.querySelector("nav").style.backgroundColor = "#285467";
+  document.querySelector("nav ul .btn button").style.backgroundColor =
+    "#eceff1";
+
+  //   console.log(window.scrollY);
+  document.querySelector("nav").classList.add("go-top");
+  if (window.scrollY > window.innerHeight - 58) {
+    document.querySelector("nav").classList.remove("go-top");
+  }
+});
+
+[...document.querySelectorAll("section")].forEach((sec) => {
+  sec.style.height = window.innerHeight + "px";
+  console.log(sec);
+});
+
+console.log(window.innerHeight);
+let sectionActual = 2;
+
+window.addEventListener("mousemove", (e) => {
+  if (window.scrollY < window.innerHeight) {
+    document.querySelector("nav").classList.add("go-top");
+  }
+  if (e.screenY < 170 && window.scrollY < window.innerHeight) {
+    document.querySelector("nav").classList.remove("go-top");
+    document.querySelector("nav").style.backgroundColor = "#00000011";
+    console.log(document.querySelector("nav ul .btn"));
+    document.querySelector("nav ul .btn button").style.backgroundColor =
+      "#00000011";
+  }
+});
+
+const button = document.querySelector(".go-down");
+button.addEventListener("click", (e) => {
+  console.log("aaaa");
+
+  window.scrollTo({
+    top: window.innerHeight,
+    behavior: "smooth",
+  });
+  document.querySelector("nav").classList.add("go-top");
+  if (window.scrollY > window.innerHeight) {
+    document.querySelector("nav").classList.remove("go-top");
+  }
+});
+
+const scrolear = document.querySelector(".btn-contact");
+scrolear.addEventListener("click", (e) => {
+  console.log("bbb");
+
+  window.scrollTo({
+    top: window.innerHeight * 4,
+    behavior: "smooth",
+  });
+});
+
+//script map
+const mapa = document.querySelector("map");
+
+let map = L.map("map").setView([-34.906744887628925, -57.94279074422511], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+let LabUnitec = L.icon({
+  iconUrl: "/src/img/Logo Unitec.png",
+
+  iconSize: [35, 35], // size of the icon
+  iconAnchor: [30, 30], // point of the icon which will correspond to marker's location
+  popupAnchor: [-15, -35], // point from which the popup should open relative to the iconAnchor
+});
+
+L.marker([-34.9066305063711, -57.94279074422511], { icon: LabUnitec })
+  .addTo(map)
+  .bindPopup("Laboratorio Principal")
+  .openPopup();
