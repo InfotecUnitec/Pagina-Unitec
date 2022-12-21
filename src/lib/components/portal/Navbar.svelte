8952ddf03fa53558c21ab485002a534d9a6f35db
Camilo Di Paolo <camilodipaolo10@gmail.com>
2022-12-21 16:25:08 -0300

    nada q valga la pena aclarar

---
+++ src/lib/components/portal/Navbar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	const menus = [
+		{
+			name: 'juan',
+			href: '#'
+		},
+		{
+			name: 'juan',
+			href: '#'
+		},
+		{
+			name: 'juan',
+			href: '#'
+		}
+	];
+</script>
+
+<nav class=" flex w-full h-24 justify-between items-center bg-slate-800">
+	<div class="ml-10">
+		<a href="/"><img src="/unitec.svg" class="w-14" alt="logo de unitec" /></a>
+	</div>
+	<ul class="flex grow justify-center gap-10 text-white">
+		{#each menus as menu}
+			<li>
+				<a href={menu.href} class="hover:text-sky-300">{menu.name}</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
---
+++ src/routes/(portal)/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Navbar from '$lib/components/portal/Navbar.svelte';
+	import Footer from '$lib/components/portal/Footer.svelte';
+	import SectionHero from '$lib/components/portal/SectionHero.svelte';
+</script>
+
+<Navbar />
+<main>
+	<SectionHero />
+</main>
+<Footer />
