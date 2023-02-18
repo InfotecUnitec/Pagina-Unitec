7966d65c97e2d12284f0086d67126cecb450a862
Camilo Di Paolo <camilodipaolo10@gmail.com>
2023-02-18 16:51:15 -0300

    footer

--- src/lib/components/portal/Footer.svelte
+++ src/lib/components/portal/Footer.svelte
@@ -1,1 +1,5 @@
-<footer class="h-52 max-w-screen bg-gray-800">footer</footer>
+<footer class="h-32 max-w-screen border-t border-stone-300 bg-stone-200 p-4">
+	<p class="font-medium text-stone-700">Contacto</p>
+	<p class="font-light text-stone-700">Email: uniteconline@gmail.com unitec@ing.unlp.edu.ar</p>
+	<p class="font-light text-stone-700">Tel: (+54) (221) 425-8911 - Interno 3508</p>
+</footer>
